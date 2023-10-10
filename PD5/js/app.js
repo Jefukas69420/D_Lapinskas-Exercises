b096d8157d0b98433f709fdfcb1bf5797656899c
Dominykas <domis1021@gmail.com>
2023-10-10 10:08:25 +0300

    commit

---
+++ PD5/js/app.js
@@ -0,0 +1,44 @@
+let skaicius = 12;
+console.log(skaicius)
+let didelisskaicius = 9007199254740991;
+console.log(didelisskaicius)
+let siulas = "sw";
+console.log(siulas)
+let taiparne = false;
+console.log(taiparne)
+let nepasakytas;
+console.log(nepasakytas)
+let niekas = null;
+console.log(niekas)
+let objektas = {
+    dydis:"didelis",
+    greitis:"greitas"
+};
+console.log(objektas)
+
+let number = 12;
+let otherNumber = 45.89;
+let textOne = 'Beautiful JavaScript World';
+let textTwo = "Beautiful JavaScript World";
+let textThree = `Beautiful JavaScript World`;
+let isTrue = (true == false);
+let isEqual = (2 >= 0);
+let petName = undefined;
+let petAge = null;
+let human = {
+humanName: undefined,
+humanAge: null
+};
+console.log(typeof number)
+console.log(typeof otherNumber)
+console.log(typeof textOne)
+console.log(typeof textTwo)
+console.log(typeof textThree)
+console.log(typeof isTrue)
+console.log(typeof isEqual)
+console.log(typeof petName)
+console.log(typeof petAge)
+console.log(typeof human)
+
+
+
