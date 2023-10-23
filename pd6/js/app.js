89541f24eadbb267c48a2e596750feed3b4fe345
Dominykas <domis1021@gmail.com>
2023-10-24 01:52:16 +0300

    added code

---
+++ pd6/js/app.js
@@ -0,0 +1,28 @@
+let funnyNumber = 12345; // Convert to String
+let funnyString = '12345'; // Convert to Number
+let funnyTrue = '1'; // Convert to Boolean
+let funnyFalse = '0'; // Convert to Boolean
+let isTrue = false; // Convert to String
+let isFalse = true; // Convert to Number
+let funnyText = 'JavaScript is dynamic programming language.'; // Convert to Number
+let funnyNull = null; // Convert to String
+let funnyUndefined = undefined; // Convert to String
+let issTrue = true; // Convert to String
+let issFalse = false; // Convert to String
+let booleanString = 'true'; // Convert to Number
+let otherBooleanString = 'false'; // Convert to Boolean
+
+
+console.log(typeof(String(funnyNumber)), String(funnyNumber));
+console.log(typeof(Number(funnyString)), Number(funnyString));
+console.log(typeof(Boolean(funnyTrue)), Boolean(funnyTrue));
+console.log(typeof(Boolean(funnyFalse)), Boolean(funnyFalse));
+console.log(typeof(String(isTrue)), String(isTrue));
+console.log(typeof(Number(isFalse)), Number(isFalse));
+console.log(typeof(Number(funnyText)), Number(funnyText));
+console.log(typeof(String(funnyNull)), String(funnyNull));
+console.log(typeof(String(funnyUndefined)), String(funnyUndefined));
+console.log(typeof(String(issTrue)), String(issTrue));
+console.log(typeof(String(issFalse)), String(issFalse));
+console.log(typeof(Number(booleanString)), Number(booleanString));
+console.log(typeof(Boolean(otherBooleanString)), Boolean(otherBooleanString));
